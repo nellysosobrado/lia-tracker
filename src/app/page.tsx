0a8f7cb3836f7e97d7999c5c09a04f1a1bdf3b48
nellysosobrado <Nelly.sosobrado@gmail.com>
2026-05-13 11:25:59 +0200

    Connected Next.Js app to supabase database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,226 +1,46 @@
 "use client"
 
-import {useEffect, useState} from "react"
+import {useEffect , useState} from "react"
+import {supabase } from "../lib/supabase"
 
-type User ={
+
+type Company = {
   id: number
-  name: string 
-  email: string
+  name: string
+  industry: string
 }
 
-export default function Home() {
-  const [users,setUsers] = useState<User[]>([])
+export default function Home(){
+  const [companies, setCompanies] = useState<Company[]>([])
 
   useEffect(() => {
-    async function loadUsers(){
-      const response = await fetch("https://jsonplaceholder.typicode.com/users")
-      const data = await response.json()
-      setUsers(data)
+    async function loadCompanies(){
+      const {data,error}  = await supabase
+      .from("companies")
+      .select("*")
+
+      if(error){
+        console.error("Supabase error:", error)
+        alert(error.message)
+        return
+      }
+
+      setCompanies(data)
     }
 
-    loadUsers()
+    loadCompanies()
   }, [])
 
-  return (
+  return(
     <main>
-      <h1>Users</h1>
-
-      {users.map((user)  => (
-        <div key={user.id}>
-          <h2>{user.name}</h2>
-          <p>{user.email} </p>
+      <h1> Companes</h1>
+      {companies.map((company) => (
+        <div key={company.id}>
+          <h2>{company.name}</h2>
+          <p>{company.industry}</p>
         </div>
       )
-
       )}
     </main>
   )
-
-  
 }
-
-// "use client"
-
-// import {useState} from "react"
-
-// export default function Home(){
-//   const[name, setName] = useState("")
-//   const[companyName, setCompanyName] = useState("")
-
-//   function handleSubmit(e: React.FormEvent){
-//     e.preventDefault()
-// alert(`Hello ${name} and ${companyName}`)
-//   }
-
-// return(
-// <main>
-//   <h1>Simple form</h1>
-
-//   <form onSubmit={handleSubmit}>
-//     <input 
-//     type="text"
-//     placeholder="Enter your name"
-//     value={name}
-//     onChange={(e) => setName(e.target.value)}
-//      />
-
-//      <input
-//      type="text"
-//      placeholder= "Enter your company"
-//      value={companyName}
-//      onChange={(e) => setCompanyName(e.target.value)}
-//      />
-
-//      <button type="submit">
-//       Submit
-//      </button>
-
-//   </form>
-// </main>
-
-
-// )
-
-// }
-
-
-// // import Greeting from "@/components/Greeting"
-
-// "use client"
-
-// import {useState} from "react"
-
-// export default function Home(){
-//   const [count,setCount]  = useState(0)
-
-//   return (
-//     <main>
-//       <h1>Counter: {count}</h1>
-
-//       <button onClick={() => setCount(count+1)}>Click </button>
-//     </main>
-//   )
-// }
-
-
-// export default function Home(){
-//   const people= [
-//     {
-//       name: "Nelly",
-//       role: "Junior Dev"
-//     },
-//     {
-//       name: "Future Nelly",
-//       role: "Senior Dev"
-//     },
-//     {
-//       name: "Recruiter",
-//       role:"Hopefully impressed"
-//     },
-//     {
-//       name: "Backend Nelly",
-//       role: "Likes c#"
-//     }
-//   ]
-
-//   return(
-//     <main>
-//       {people.map((person) =>(
-// <Greeting
-// key={person.name}
-// name={person.name}
-// role={person.role}
-// />  
-
-//       ))}
-//     </main>
-//   )
-// }
-
-// export default function Home(){
-//   return (
-//     <main>
-//       <Greeting
-//       name="Nelly"
-//       role="Junior Dev"
-//       />
-//       <Greeting name= "Me" role="tester"/>
-//     </main>
-//   )
-// }
-
-// export default function Home() {
-  
-//   const name="Nelly"
-//   const role = ".NET Developer Student"
-//   return (
-//     <main>  
-//       <h1>Hej </h1>
-//       <p>Välkommen till min app!</p>
-//       <p>{role}</p>
-//     </main>
-//   )
-
-// export default function Home() {
-//   return (
-//     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-//       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-//         <Image
-//           className="dark:invert"
-//           src="/next.svg"
-//           alt="Next.js logo"
-//           width={100}
-//           height={20}
-//           priority
-//         />
-//         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-//           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-//             To get started, edit the page.tsx file.
-//           </h1>
-//           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-//             Looking for a starting point or more instructions? Head over to{" "}
-//             <a
-//               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//               className="font-medium text-zinc-950 dark:text-zinc-50"
-//             >
-//               Templates
-//             </a>{" "}
-//             or the{" "}
-//             <a
-//               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//               className="font-medium text-zinc-950 dark:text-zinc-50"
-//             >
-//               Learning
-//             </a>{" "}
-//             center.
-//           </p>
-//         </div>
-//         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-//           <a
-//             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-//             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <Image
-//               className="dark:invert"
-//               src="/vercel.svg"
-//               alt="Vercel logomark"
-//               width={16}
-//               height={16}
-//             />
-//             Deploy Now
-//           </a>
-//           <a
-//             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-//             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Documentation
-//           </a>
-//         </div>
-//       </main>
-//     </div>
-//   );
-// }
