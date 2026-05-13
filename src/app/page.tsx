b0108eb2617b3af1bb538f1430c6c7ac872b8fa3
nellysosobrado <Nelly.sosobrado@gmail.com>
2026-05-13 11:59:40 +0200

    Added crate company form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-import {useEffect , useState} from "react"
-import {supabase } from "../lib/supabase"
-
+import {useEffect, useState} from "react"
+import {supabase} from "../lib/supabase"
 
 type Company = {
   id: number
@@ -12,35 +11,124 @@ type Company = {
 
 export default function Home(){
   const [companies, setCompanies] = useState<Company[]>([])
+  const [name, setName] = useState("")
+  const [industry, setIndustry] = useState("")
 
-  useEffect(() => {
-    async function loadCompanies(){
-      const {data,error}  = await supabase
-      .from("companies")
-      .select("*")
+  async function loadCompanies(){
+    const {data,error} = await supabase
+    .from ("companies")
+    .select("*")
+    .order("id", {ascending:true})
 
-      if(error){
-        console.error("Supabase error:", error)
-        alert(error.message)
-        return
-      }
-
-      setCompanies(data)
+    if(error){
+      alert(error.message)
+      return
     }
-
+    setCompanies(data)
+  }
+  useEffect(() => {
     loadCompanies()
-  }, [])
+  }, [] )
 
-  return(
+  async function handleSubmit(e: React.FormEvent){
+    e.preventDefault()
+
+    const{error} = await supabase
+    .from("companies")
+    .insert({
+      name: name,
+      industry: industry,
+
+    })
+    if(error){
+      alert(error.message )
+        return
+      
+    }
+    setName("")
+    setIndustry("")
+    loadCompanies() 
+
+    
+  }
+  return (
     <main>
-      <h1> Companes</h1>
+      <h1> Companies</h1>
+
+      <form onSubmit={handleSubmit}> 
+        <input
+        type= "text"
+        placeholder="Company Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        />
+
+        <input 
+        type="text"
+        placeholder="Industry"
+        value={industry}
+        onChange={(e) => setIndustry(e.target.value)}
+        />
+
+        <button type="submit"> Add company</button>
+      </form>
+
+
       {companies.map((company) => (
         <div key={company.id}>
           <h2>{company.name}</h2>
           <p>{company.industry}</p>
         </div>
-      )
-      )}
+      ) )}
     </main>
   )
 }
+
+
+
+// "use client"
+
+// import {useEffect , useState} from "react"
+// import {supabase } from "../lib/supabase"
+
+
+// type Company = {
+//   id: number
+//   name: string
+//   industry: string
+// }
+
+// export default function Home(){
+//   const [companies, setCompanies] = useState<Company[]>([])
+
+//   useEffect(() => {
+//     async function loadCompanies(){
+//       const {data,error}  = await supabase
+//       .from("companies")
+//       .select("*")
+
+//       if(error){
+//         console.error("Supabase error:", error)
+//         alert(error.message)
+//         return
+//       }
+
+//       setCompanies(data)
+//     }
+
+//     loadCompanies()
+//   }, [])
+
+//   return(
+//     <main>
+//       <h1> Companes</h1>
+//       {companies.map((company) => (
+//         <div key={company.id}>
+//           <h2>{company.name}</h2>
+//           <p>{company.industry}</p>
+//         </div>
+//       )
+//       )}
+//     </main>
+//   )
+// }
